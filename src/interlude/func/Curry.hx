caa8d0f546646ecc88922900e3b2689d8ccc8b88
Evan Montembeault <evan@emonte.net>
2021-03-28 10:46:45 -0700

    adding basic comments missing from func package

--- src/interlude/func/Curry.hx
+++ src/interlude/func/Curry.hx
@@ -21,9 +21,17 @@ class Curry2 {
 @:nullSafety(Strict)
 @:publicFields
 class Curry3 {
+    /**
+        Convert 1 function of 3 arguments into 3 functions of 1 argument  
+        `((a, b, c) -> a + b + c).curry()` == `a -> b -> c -> a + b + c`
+     **/
     static function curry<A, B, C, Z>(f:(A, B, C)->Z):A->(B->(C->Z)) return
         a -> b -> f.bind(a, b);
 
+    /**
+        Convert 3 functions of 1 argument into 1 function of 3 arguments  
+        `(a -> b -> c -> a + b + c).uncurry()` == `(a, b, c) -> a + b + c`
+    **/
     static function uncurry<A, B, C, Z>(f:A->(B->(C->Z))):A->B->C->Z return
         (a, b, c) -> f(a)(b)(c);
 }
@@ -31,9 +39,17 @@ class Curry3 {
 @:nullSafety(Strict)
 @:publicFields
 class Curry4 {
+    /**
+        Convert 1 function of 4 arguments into 4 functions of 1 argument  
+        `((a, b, c, d) -> a + b + c + d).curry()` == `a -> b -> c -> d -> a + b + c + d`
+     **/
     static function curry<A, B, C, D, Z>(f:(A, B, C, D)->Z):A->(B->(C->(D->Z))) return
         a -> b -> c -> f.bind(a, b, c);
 
+    /**
+        Convert 4 functions of 1 argument into 1 function of 4 arguments  
+        `(a -> b -> c -> d -> a + b + c + d).uncurry()` == `(a, b, c, d) -> a + b + c + d`
+    **/
     static function uncurry<A, B, C, D, Z>(f:A->(B->(C->(D->Z)))):A->B->C->D->Z return
         (a, b, c, d) -> f(a)(b)(c)(d);
 }
@@ -41,9 +57,17 @@ class Curry4 {
 @:nullSafety(Strict)
 @:publicFields
 class Curry5 {
+    /**
+        Convert 1 function of 5 arguments into 5 functions of 1 argument  
+        `((a, b, c, d, e) -> a + b + c + d + e).curry()` == `a -> b -> c -> d -> e -> a + b + c + d + e`
+     **/
     static function curry<A, B, C, D, E, Z>(f:(A, B, C, D, E)->Z):A->(B->(C->(D->(E->Z)))) return
         a -> b -> c -> d -> f.bind(a, b, c, d);
 
+    /**
+        Convert 5 functions of 1 argument into 1 function of 5 arguments  
+        `(a -> b -> c -> d -> e -> a + b + c + d + e).uncurry()` == `(a, b, c, d, e) -> a + b + c + d + e`
+    **/
     static function uncurry<A, B, C, D, E, Z>(f:A->(B->(C->(D->(E->Z))))):A->B->C->D->E->Z return
         (a, b, c, d, e) -> f(a)(b)(c)(d)(e);
 }
@@ -51,9 +75,17 @@ class Curry5 {
 @:nullSafety(Strict)
 @:publicFields
 class Curry6 {
+    /**
+        Convert 1 function of 6 arguments into 6 functions of 1 argument  
+        `((a, b, c, d, e, f) -> a + b + c + d + e + f).curry()` == `a -> b -> c -> d -> e -> f-> a + b + c + d + e + f`
+     **/
     static function curry<A, B, C, D, E, F, Z>(f:(A, B, C, D, E, F)->Z):A->(B->(C->(D->(E->(F->Z))))) return
         a -> b -> c -> d -> e -> f.bind(a, b, c, d, e);
 
+    /**
+        Convert 6 functions of 1 argument into 1 function of 6 arguments  
+        `(a -> b -> c -> d -> e -> f -> a + b + c + d + e + f).uncurry()` == `(a, b, c, d, e, f) -> a + b + c + d + e + f`
+    **/
     static function uncurry<A, B, C, D, E, F, Z>(fn:A->(B->(C->(D->(E->(F->Z)))))):A->B->C->D->E->F->Z return
         (a, b, c, d, e, f) -> fn(a)(b)(c)(d)(e)(f);
 }
--- src/interlude/func/Trampoline.hx
+++ src/interlude/func/Trampoline.hx
@@ -1,5 +1,10 @@
 package interlude.func;
 
+/**
+    Represents a recursive computation that has been broken into chunks to
+    avoid growing the stack  
+    https://en.wikipedia.org/wiki/Tail_call#Through_trampolining
+**/
 enum Trampoline<T> {
     Done(t:T);
     Continue(f:Void->Trampoline<T>);
@@ -9,7 +14,8 @@ enum Trampoline<T> {
 @:publicFields
 class TrampolineTools {
     /**
-        Runs a recursive operation as an iterative one until it has been exhausted
+        Runs a recursive operation as an iterative one until it has been
+        exhausted
     **/
     static function trampoline<T>(t:Trampoline<T>):T {
         var cache = t;
