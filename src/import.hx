36b79dd6f8c47e63a584dd43cbfdc8d0fd2c617b
Evan Montembeault <evan@emonte.net>
2021-04-03 16:13:17 -0700

    adding comments to import.hx

--- src/import.hx
+++ src/import.hx
@@ -1,6 +1,9 @@
+// Import everything
 using Interlude;
 
+// Interlude uses NotVoid in a lot of places
 import haxe.Constraints.NotVoid;
 
+// Include helpful funcions so they can be used unqualified (e.g. `identity`)
 import interlude.func.ValueTools.*;
 import interlude.func.FunctionTools.*;
