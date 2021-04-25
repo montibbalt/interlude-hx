5d53e511bbe9bca88c6ac146cae0a49aa868e1ab
Evan Montembeault <evan@emonte.net>
2021-04-25 16:24:13 -0700

    replace dashes with camelCase field names in JsonProvider

--- src/interlude/macro/JsonProvider.hx
+++ src/interlude/macro/JsonProvider.hx
@@ -1,8 +1,11 @@
 package interlude.macro;
 
+import haxe.macro.Expr;
+
 using haxe.Http;
-using haxe.Json;
 using haxe.macro.Context;
+using haxe.Json;
+using Reflect;
 using sys.io.File;
 
 /**
@@ -16,50 +19,45 @@ using sys.io.File;
 
     ...
 
-    var a:Example = {}; // Object requires field foo
-    var b:Example = { foo: "bar" }; // String should be Float
+    var a:Example = {}; // Compile error: Object requires field foo
+    var b:Example = { foo: "bar" }; // Compile error: String should be Float
     ```
 **/
 class JsonProvider {
     public static macro function sampleString(json:String):Array<Field> return
-        json
-            .parse()
-            .combineFields();
+        combineFields(json.parse());
 
     public static macro function sampleFile(jsonPath:String):Array<Field> return
-        jsonPath
-            .getContent()
-            .parse()
-            .combineFields();
+        combineFields(jsonPath.getContent().parse());
 
     public static macro function sampleWeb(url:String):Array<Field> return
-        url
-            .requestUrl()
-            .parse()
-            .combineFields();
+        combineFields(url.requestUrl().parse());
 
 #if macro
+    /** Appends the fields from some object to our existing type's fields **/
     static function combineFields(dyn:Dynamic):Array<Field> return
         Context
             .getBuildFields()
             .concat(fromDynamic(dyn));
 
-    static function fromDynamic(dyn:Dynamic):Array<Field> return {
-        var fields = Reflect.fields(dyn);
-
-        fields.map(fs -> {
-            var value = Reflect.field(dyn, fs);
-            var type = Context
-                .typeof(macro $v{value})
-                .toComplexType();
-
-            {   name    : fs
-            ,   pos     : Context.currentPos()
-            ,   kind    : FVar(type, null)
-            ,   access  : [APublic]
-            ,   doc     : null
-            ,   meta    : null }
+    /** Extracts all of the `Field`s from some object **/
+    static function fromDynamic(dyn:Dynamic):Array<Field> return
+        dyn.fields().map(fieldName -> {
+            name    : fixDashes(fieldName)
+        ,   pos     : Context.currentPos()
+        ,   kind    : FVar(Context.typeof(macro $v{dyn.field(fieldName)}).toComplexType(), null)
+        ,   access  : [APublic]
+        ,   doc     : null
+        ,   meta    : null
         });
-    }
+
+    /**
+        Convert dashed-field-names to camelCase
+        ```haxe
+        fixDashes('foo-bar-Baz----Qux-1') == 'fooBarBazQux1';
+        ```
+    **/
+    static function fixDashes(dashedInput:String):String return
+        (~/\-+([a-z]?)/g).map(dashedInput, reg -> reg.matched(1).toUpperCase());
 #end
 }
